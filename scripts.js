a29eb13504d88acaf87d329ce8dcbd151abe6070
Evgen0601 <63299293+Evgen0601@users.noreply.github.com>
2020-06-06 15:05:18 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,32 @@
+let  money  =  prompt ( "Ваш бюджет на месяц?" ) ;
+let  time  =  prompt ( "Введите дату в формате YYYY-MM-DD" ) ;
+пусть  расходы  =  { } ;
+let  appData  =  {
+    бюджет : деньги ,
+    timeData : время ,
+    расходы : { } ,
+    необязательные расходы : { } ,
+    доход : [ ] ,
+    moneyPerDay : 0 ,
+    Экономия : ложная
+} ;
+для ( пусть  i  =  0 ;  i < 2 ;  i ++ ) {
+    let  a  =  prompt ( "Введите обязательную статью расходов в этом месяце" ) ;
+    let  b  =  prompt ( "Во сколько обойдется?" ) ;
+
+    if ( ( typeof ( a ) ) === 'string'  &&  ( typeof ( a ) ) ! = null &&  ( typeof ( b ) ) ! = null &&  a ! = ''  &&  b ! = ''  &&  a . length < 50 ) {
+        appData . расходы [ а ]  =  б ;
+    }
+}
+appData . moneyPerDay  =  appData . бюджет / 30 ;
+alert ( "Ежедневный бюджет:"  +  appData . moneyPerDay ) ;
+if ( appData . moneyPerDay  <  100 ) {
+    консоль . log ( «Минимальный уровень достатка» ) ;
+} else  if ( appData . moneyPerDay  >  100  &&  appData . moneyPerDay  <  2000 ) {
+    консоль . log ( "Средний уровень достатка" ) ;
+} else  if ( appData . moneyPerDay  >  2000 ) {
+    консоль . log ( "Высокий уровень достатка" ) ;
+} еще {
+    консоль . log ( "Произошла ошибка" ) ;
+}
+консоль . журнал ( «сделано» ) ;
